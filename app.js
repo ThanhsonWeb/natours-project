d2c69cc1b629322a9ac28a50a4435c4080ab92d1
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-26 19:21:48 +0700

    Setting up Express and basic Routing

--- app.js
+++ app.js
@@ -2,9 +2,14 @@
 const express = require('express');
 
 const app = express();
-
+// Get
 app.get('/', (req, res) => {
-  res.status(200).send('hello ');
+  res.status(200).json({ message: 'hello', app: 'Natours' });
+});
+
+//POST
+app.post('/', (req, res) => {
+  res.send('you can post to this endpoint... ');
 });
 
 app.listen(3000, () => {
