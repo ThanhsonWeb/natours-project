4ac989eaa3a3ae2d7ad57e34188cc27dd95f201f
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-27 19:56:03 +0700

    handling post request

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-
 const express = require('express');
 const fs = require('fs');
 
 const app = express();
+// simple middleware
+app.use(express.json());
 
 // b1 : Ensure it's JavaScript object.
 const tours = JSON.parse(
@@ -18,6 +19,28 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+// create new one
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1; // 9
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  // overwrite this file
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
 });
 
 app.listen(3000, () => {
