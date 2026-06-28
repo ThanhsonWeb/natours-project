a617bc0b888df0fe1c430b17532bc37e8823e391
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-28 08:42:34 +0700

    handling PATCH and DELETE request

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const app = express();
 // simple middleware
 app.use(express.json());
 
-// b1 : Ensure it's JavaScript object.
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
@@ -20,6 +19,27 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = Number(req.params.id);
+
+  const tour = tours.find((el) => el.id === id); // get exact tour
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+  // here
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
 // create new one
 app.post('/api/v1/tours', (req, res) => {
   console.log(req.body);
@@ -41,6 +61,36 @@ app.post('/api/v1/tours', (req, res) => {
       });
     },
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Update tour here....>',
+    },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 });
 
 app.listen(3000, () => {
