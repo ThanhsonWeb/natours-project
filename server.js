24c0193bff93695b3853d3c6a2c078b89ae626d2
ThanhsonWeb <sondeptroi@gmail.com>
2026-07-08 17:09:14 +0700

    Connect our database with Express Ap

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-const app = require('./app');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+	"<PASSWORD>",
+	process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+	// .connect(process.env.DATABASE_LOCAL, {
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		console.log("DB Connection Successfully");
+	});
 
 // console.log(process.env);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+	console.log(`App running on port ${port}...`);
 });
