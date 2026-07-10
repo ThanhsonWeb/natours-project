9eb346acfc8e32c1c6acdbec100544c8af2c40c6
ThanhsonWeb <sondeptroi@gmail.com>
2026-07-10 10:45:24 +0700

    Making API better with Advanced Filtering

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 
-const morgan = require('morgan');
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
+const morgan = require("morgan");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
+app.set("query parser", "extended");
 
 // 1
 console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
 }
 
 app.use(express.json());
@@ -19,18 +20,18 @@ app.use(express.static(`${__dirname}/public`));
 
 // Middleware apply to  every request
 app.use((req, res, next) => {
-  console.log('hello from middleware 🌟');
-  next();
+	console.log("hello from middleware 🌟");
+	next();
 });
 
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();                                                                          
-  console.log(req.requestTime);
-  next();
+	req.requestTime = new Date().toISOString();
+	console.log(req.requestTime);
+	next();
 });
 
 // 2. Routes
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
 module.exports = app;
