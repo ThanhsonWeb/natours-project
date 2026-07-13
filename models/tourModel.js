c1e9a15e5dae7d640b57314e4b20e53ecb1cdec2
ThanhsonWeb <sondeptroi@gmail.com>
2026-07-13 19:58:38 +0700

    built in validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,8 +4,12 @@ const tourSchema = new mongoose.Schema(
 	{
 		name: {
 			type: String,
-			required: [true, "A tour must have a name"], //validate
+			required: [true, "A tour must have a name"],
+			unique: true,
 			trim: true,
+			// built in validators
+			maxlength: [40, "A tour name must have less or equal then 40 characters"],
+			minlength: [10, "A tour name must have less or equal then 40 characters"],
 		},
 		slug: String,
 
@@ -22,11 +26,17 @@ const tourSchema = new mongoose.Schema(
 		difficulty: {
 			type: String,
 			required: [true, "A tour must have a difficulty"], //validate
+			enum: {
+				values: ["easy", "medium", "difficult"],
+				message: "Difficulty is either: easy, medium, difficult",
+			},
 		},
 
 		ratingsAverage: {
 			type: Number,
 			default: 4.5,
+			min: [1, "Rating must be above 1.0"],
+			max: [5, "Rating must be less than or equal to 5.0"],
 		},
 
 		ratingsQuantity: {
@@ -114,12 +124,12 @@ tourSchema.pre("aggregate", function (next) {
 
 	// unshift = insert at the front
 	// pipeline  is
-//   { '$match': { secretTour: [Object] } },
-//   { '$match': { ratingsAverage: [Object] } },
-//   { '$group': {
-//       _id: '$difficulty',}
-//   },
-//   { '$sort': { avgRating: -1 } }
+	//   { '$match': { secretTour: [Object] } },
+	//   { '$match': { ratingsAverage: [Object] } },
+	//   { '$group': {
+	//       _id: '$difficulty',}
+	//   },
+	//   { '$sort': { avgRating: -1 } }
 
 	this.pipeline().unshift({
 		$match: { secretTour: { $ne: true } },
