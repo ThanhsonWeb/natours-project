a78221a742bff0d8c09f72521d4d04590a33af09
ThanhsonWeb <sondeptroi@gmail.com>
2026-07-15 20:11:33 +0700

    Errors during development and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,37 @@
+const sendErrorDev = (err, res) => {
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message,
+		// more info for devs
+		stack: err.stack,
+		error: err,
+	});
+};
+
+const sendErrorProd = (err, res) => {
+	// error for client (Expected error)
+	if (err.isOPerational) {
+		res.status(err.statusCode).json({
+			status: err.status,
+			message: err.message,
+		});
+	} else {
+		// error fo
+		console.log("ERROR 🌋")
+
+		res.status(500).json({
+			status: "err",
+			message: " Something went wrong"
+		});
+	}
+};
 module.exports = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;
 	err.status = err.status || "error";
 
-	res.status(err.statusCode).json({
-		status: err.status,
-		message: err.message,
-	});
+	if (process.env.NODE_ENV === "development") {
+		sendErrorDev(err, res);
+	} else if (process.env.NODE_ENV === "production") {
+		sendErrorProd(err, res);
+	}
 };
