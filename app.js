0c642e258688bd24ba88f5487a2b38aa8086751b
ThanhsonWeb <sondeptroi@gmail.com>
2026-07-15 15:01:02 +0700

    Refactoring errorcontrollers

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
+const globalErrorHandling = require("./controllers/errorController");
+const AppError = require("./utils/appError");
 
 const app = express();
 app.set("query parser", "extended");
@@ -14,37 +16,17 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-	req.requestTime = new Date().toISOString();
-	console.log(req.requestTime);
-	next();
-});
-
 // 2. Routes
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
-// all = all HTTP request
+
+// user visit wrong route
 app.all("*", (req, res, next) => {
-	// res.status(404).json({
-	// 	status: "fail",
-	// 	message: `Can't find ${req.originalUrl} on this server`,
-	// });
-
-	const err = new Error(`Can't find ${req.originalUrl} on this server`);
-	err.status = "fail !";
-	err.statusCode = 404;
-	next(err);
+	// create an instance of the class
+	next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
-// global error handling middleware 
-app.use((err, req, res, next) => {
-	err.statusCode = err.statusCode || 500;
-	err.status = err.status || "error";
-
-	res.status(err.statusCode).json({
-		status: err.status,
-		message: err.message,
-	});
-});
+// global error handling middleware
+app.use(globalErrorHandling);
 
 module.exports = app;
