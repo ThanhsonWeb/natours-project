76259e2be558559d7fcd2c788ad49c994e830a5f
ThanhsonWeb <sondeptroi@gmail.com>
2026-07-16 14:52:00 +0700

    Hanlding Invalid Database Ids and Duplicate database fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,37 +1,61 @@
+const AppError = require("../utils/appError");
+// nice friendly error for users
+const handleCastErrorDB = (err) => {
+	// path = _id  || value = wwwww
+	const message = `Invalid ${err.path} : ${err.value}`;
+	return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+	const value = Object.values(err.keyValue)[0];
+	const message = `Duplicate field value: "${value}". Use another value.`;
+	return new AppError(message, 400);
+};
+
+
+
 const sendErrorDev = (err, res) => {
 	res.status(err.statusCode).json({
 		status: err.status,
+		error: err,
 		message: err.message,
 		// more info for devs
 		stack: err.stack,
-		error: err,
 	});
 };
 
 const sendErrorProd = (err, res) => {
 	// error for client (Expected error)
-	if (err.isOPerational) {
+	if (err.isOperational) {
 		res.status(err.statusCode).json({
 			status: err.status,
 			message: err.message,
 		});
 	} else {
-		// error fo
-		console.log("ERROR 🌋")
+		console.log("ERROR 🌋");
 
 		res.status(500).json({
 			status: "err",
-			message: " Something went wrong"
+			message: " Something went very wrong",
 		});
 	}
 };
 module.exports = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;
-	err.status = err.status || "error";
+	err.status = err.status || "error ok";
 
 	if (process.env.NODE_ENV === "development") {
 		sendErrorDev(err, res);
 	} else if (process.env.NODE_ENV === "production") {
-		sendErrorProd(err, res);
+		let error = err;
+		// name : "CastError" and code: 11000 from our dev mode error
+		//  create conditional base on error in dev mode
+		if (error.name === "CastError") {
+			error = handleCastErrorDB(error);
+		}
+		if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+		
+
+		sendErrorProd(error, res);
 	}
 };
