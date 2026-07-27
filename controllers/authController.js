15283d1c27d792350d85eb791cae6dc90c8924c3
ThanhsonWeb <sondeptroi@gmail.com>
2026-07-27 08:12:33 +0700

    Sending email with nodemailer p-1

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const jwt = require("jsonwebtoken");
 const AppError = require("../utils/appError");
+const sendEmail = require("../utils/email");
 
 const signToken = (id) => {
 	return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -120,8 +121,25 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 	}
 
 	// 2. Generate the random reset token
-	const resetToken = user.createPasswordResetToken();
-	await user.save();
+	const resetToken = user.createPasswordResetToken(); //abc23dfo
+
+	await user.save({ validateBeforeSave: false });
+
+	// 3. Send it to user's email
+	const resetURL = `${req.protocol}://${req.get("host")}/api/v1/users/resetPassword/${resetToken}`;
+
+	const message = `Forgot your password? Submit a PATCH req with your new password to : ${resetURL} , If you didn't request password token,  please ignore this email `;
+
+	await sendEmail({
+		email: user.email,
+		subject: "your password reset token (valid for 10 mins )",
+		message,
+	});
+
+	res.status(200).json({
+		status: "success",
+		message: "Token sent to email ^^",
+	});
 });
 exports.resetPassword = (req, res, next) => {
 	console.log("hello");
